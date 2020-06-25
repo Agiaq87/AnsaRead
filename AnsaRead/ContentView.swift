097e8f17fc5982c729fe0c9483165390c54d8ba7
Alessandro Giaquinto <agiaquinto87@recensility.it>
2020-06-25 18:52:27 +0200

    Initial Commit

---
+++ AnsaRead/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AnsaRead
+//
+//  Created by Alessandro Giaquinto on 25/06/2020.
+//  Copyright © 2020 Alessandro Giaquinto. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ AnsaReadTests/AnsaReadTests.swift
@@ -0,0 +1,34 @@
+//
+//  AnsaReadTests.swift
+//  AnsaReadTests
+//
+//  Created by Alessandro Giaquinto on 25/06/2020.
+//  Copyright © 2020 Alessandro Giaquinto. All rights reserved.
+//
+
+import XCTest
+@testable import AnsaRead
+
+class AnsaReadTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
